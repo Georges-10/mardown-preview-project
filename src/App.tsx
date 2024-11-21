881cc2764da3a1abed3a0e868d602aba200d2c23
Georges10 <neiladibet@gmail.com>
2024-11-22 00:49:22 +0100

    remove second props (controlled): value

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,9 @@ function App() {
     setTextMarkDown(textAreaRef.current?.value);
   };
   useEffect(() => {
-    handleChange();
-
     if (textAreaRef.current) {
-      textAreaRef.current?.focus();
+      setTextMarkDown(textAreaRef.current.defaultValue);
+      textAreaRef.current.focus();
     }
   }, []);
 
@@ -26,7 +25,6 @@ function App() {
           ref={textAreaRef}
           defaultValue="# `Prévisualisateur - Georges Neil`&#13;### Oh ! De la magie !&#13;En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !&#13;&#13;### Cas d'utilisation&#13;* *italique*&#13;* **gras**&#13;* `monospace`&#13;* ~~barré~~&#13;* # h1&#13;* ## h2&#13;* ### h3&#13;* #### etc&#13;[Georges Neil Github](https://github.com/Georges-10)"
           onChange={handleChange}
-          value={textMarkdown}
         ></textarea>
         <div className="element">
           <Markdown>{textMarkdown}</Markdown>
