6ddbfa77a0fa147a1faa9226ff0489715edf3ff9
Georges10 <neiladibet@gmail.com>
2024-11-21 19:57:35 +0100

    Mardown  preview Project in Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import Markdown from "marked-react";
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [textMarkdown, setTextMarkDown] = useState<string | undefined>("");
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleChange = () => {
+    setTextMarkDown(textAreaRef.current?.value);
+  };
+  useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current?.focus();
+    }
+    handleChange();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="elements">
+        <textarea
+          rows={30}
+          className="element"
+          ref={textAreaRef}
+          defaultValue="# `Prévisualisateur - Believemy`&#13;### Oh ! De la magie !&#13;En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !&#13;&#13;### Cas d'utilisation&#13;* *italique*&#13;* **gras**&#13;* `monospace`&#13;* ~~barré~~&#13;* # h1&#13;* ## h2&#13;* ### h3&#13;* #### etc&#13;[Believemy](https://believemy.com)"
+          onChange={handleChange}
+          value={textMarkdown}
+        ></textarea>
+        <div className="element">
+          <Markdown>{textMarkdown}</Markdown>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
