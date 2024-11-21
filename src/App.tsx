9e638e9cb8f9b923a9abe2b9b64c43514fd0047a
Georges10 <neiladibet@gmail.com>
2024-11-21 20:03:10 +0100

    Changing the default message value

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           rows={30}
           className="element"
           ref={textAreaRef}
-          defaultValue="# `Prévisualisateur - Believemy`&#13;### Oh ! De la magie !&#13;En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !&#13;&#13;### Cas d'utilisation&#13;* *italique*&#13;* **gras**&#13;* `monospace`&#13;* ~~barré~~&#13;* # h1&#13;* ## h2&#13;* ### h3&#13;* #### etc&#13;[Believemy](https://believemy.com)"
+          defaultValue="# `Prévisualisateur - Georges Neil`&#13;### Oh ! De la magie !&#13;En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !&#13;&#13;### Cas d'utilisation&#13;* *italique*&#13;* **gras**&#13;* `monospace`&#13;* ~~barré~~&#13;* # h1&#13;* ## h2&#13;* ### h3&#13;* #### etc&#13;[Georges Neil Github](https://github.com/Georges-10)"
           onChange={handleChange}
           value={textMarkdown}
         ></textarea>
